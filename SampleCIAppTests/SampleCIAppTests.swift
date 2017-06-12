677990b3d9e2ede0bea57b82f4f06bfa53bfb312
Aleksandrs Proskurins <aleksandrs.proskurins@ask.fm>
2017-06-12 11:15:21 +0300

    Files

---
+++ SampleCIAppTests/SampleCIAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SampleCIAppTests.swift
+//  SampleCIAppTests
+//
+//  Created by Aleksandrs Proskurins on 12/06/2017.
+//  Copyright © 2017 Aleksandr Proskurin. All rights reserved.
+//
+
+import XCTest
+@testable import SampleCIApp
+
+class SampleCIAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+
+        XCTFail("Test failed")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
